a129c23a26241543704382e061c14494a424e2d5
Mohd Kashan <kashan0716@gmail.com>
2026-06-10 15:50:31 +0530

    full

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,16 +1,19 @@
 import axios from "axios";
 
+// Change only these URLs when needed
+const SERVER_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:3000/api"
+    : "/api";
+
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: SERVER_URL,
 });
 
-// Task API functions
+// Task APIs
 export const addTask = (task) => API.post("/tasks", { task });
-
 export const getTasks = () => API.get("/tasks");
-
 export const deleteTask = (id) => API.delete(`/tasks/${id}`);
-
 export const updateTask = (id, data) => API.put(`/tasks/${id}`, data);
 
 export default API;
